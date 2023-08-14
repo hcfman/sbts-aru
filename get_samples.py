533b93c863b1e075f543ac0feddaf5b551d8e162
Kim Hendrikse <kim@kiwiclan.com>
2023-08-14 19:42:18 +0200

    Add utility for determining the number of samples in a .flac file

--- get_samples.py
+++ get_samples.py
@@ -4,15 +4,15 @@ import soundfile as sf
 import sys
 
 def get_samples(file_name):
-    # Open the flac file
     data, samplerate = sf.read(file_name)
-    # Total number of samples is simply the size of the array
+
+    # Number of samples the size of the array
     samples = data.shape[0]
     return samples
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python3 script.py <flac_file_path>")
+        print("Usage: get_samples.py <flac_file_path>")
         return
 
     flac_file_path = sys.argv[1]
