7f2ee6c73c6e6dd20583b35d9dc9a1a07f0bf993
Kim Hendrikse <kim@kiwiclan.com>
2023-09-14 16:08:31 +0200

    Add time_diffs.py

---
+++ time_diffs.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import sys
+import argparse
+from datetime import datetime
+
+def main():
+    # Initialize argparse
+    parser = argparse.ArgumentParser(description="Calculate time differences from the earliest time.")
+    parser.add_argument("-p", "--python_array", action="store_true",
+                        help="Output the time differences as a Python array of floats.")
+    args = parser.parse_args()
+
+    # Initialize variables
+    input_times = []
+    time_differences = []
+
+    # Read lines from stdin
+    for line in sys.stdin:
+        line = line.strip()
+        input_times.append(datetime.strptime(line, "%Y-%m-%d_%H-%M-%S.%f"))
+
+    # Find the earliest time
+    earliest_time = min(input_times)
+
+    # Calculate the time differences
+    for t in input_times:
+        delta = t - earliest_time
+        time_diff = delta.total_seconds()
+        time_differences.append(time_diff)
+
+    # Output the results
+    if args.python_array:
+        print(time_differences)
+    else:
+        for time_diff in time_differences:
+            print(f"{time_diff:.6f}")
+
+if __name__ == "__main__":
+    main()
