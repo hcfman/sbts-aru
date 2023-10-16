a3c2d02f9d22f297d7f4ee0c74a615ee1e977c8e
Kim Hendrikse <kim@kiwiclan.com>
2023-10-16 19:25:47 +0200

    Fix create_fdisk_cmds.py and add specifying for the swap partition

--- create_fdisk_cmds.py
+++ create_fdisk_cmds.py
@@ -4,75 +4,67 @@ import subprocess
 import sys
 import math
 
-def get_used_space_of_root():
-    cmd = ["df", "-k", "/"]
-    output = subprocess.check_output(cmd).decode('utf-8')
-    
-    for line in output.splitlines():
-        if "/" in line:
-            parts = line.split()
-            used_kb = int(parts[2])
-            print(f"DEBUG: Used space in KB: {used_kb}")
-            return used_kb * 1024
-    raise Exception("Unable to determine used space from df command.")
+def get_used_space():
+    result = subprocess.run(['df', '-k', '/'], capture_output=True, text=True)
+    for line in result.stdout.splitlines():
+        if '/dev/root' in line:
+            used_space_kb = int(line.split()[2])
+            return used_space_kb
+    raise Exception("Couldn't determine the used space on the root partition.")
 
-def generate_fdisk_commands(start_block, extra_gb):
-    current_bytes = get_used_space_of_root()
-    print(f"DEBUG: Current used bytes: {current_bytes}")
-    
-    total_bytes = current_bytes + (extra_gb * (1024**3))
-    print(f"DEBUG: Total bytes including extra space: {total_bytes}")
-    
-    total_blocks = math.ceil(total_bytes / 512)
-    print(f"DEBUG: Total blocks (rounded up): {total_blocks}")
-    
-    end_block_2 = start_block + total_blocks - 1
-    print(f"DEBUG: End block for partition 2: {end_block_2}")
-    
-    # Calculate the start and end blocks for partition 3
-    start_block_3 = end_block_2 + 1
-    end_block_3 = start_block_3 + 4 * (1024**3 // 512) - 1  # +1 GB in blocks
+if len(sys.argv) != 4:
+    print("Usage: script_name <start_block_partition2> <extra_gb_partition2> <gb_partition3>")
+    sys.exit(1)
 
-    commands = [
-        'd',
-        '2',
-        'n',
-        'p',
-        '2',
-        str(start_block),
-        str(end_block_2),
-        'No',
-        'n',
-        'p',
-        '3',
-        str(start_block_3),
-        str(end_block_3),
-        'n',
-        'e',
-        str(end_block_3 + 1),
-	''
-        'n',
-        '',
-        '+512M',
-        'n',
-        '',
-	'',
-        't',
-        '3',
-        '82',
-        'w'
-    ]
+start_block_partition2 = int(sys.argv[1])
+extra_gb_partition2 = int(sys.argv[2])
+gb_partition3 = int(sys.argv[3])
 
-    return commands
+# Convert GB to KB
+extra_kb_partition2 = extra_gb_partition2 * 1024 * 1024
+kb_partition3 = gb_partition3 * 1024 * 1024
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python3 script_name.py <start_block> <extra_gb>")
-        sys.exit(1)
-    
-    start_block = int(sys.argv[1])
-    extra_gb = int(sys.argv[2])
-    
-    commands = generate_fdisk_commands(start_block, extra_gb)
-    for cmd in commands:
-        print(cmd)
+# Determine current used space and calculate total required KB for partition 2
+used_space_kb = get_used_space()
+total_required_kb_partition2 = used_space_kb + extra_kb_partition2
+
+# Round up the total required KB of partition 2 to the nearest GB and convert to sectors
+rounded_gb_partition2 = math.ceil(total_required_kb_partition2 / (1024 * 1024))
+sectors_required_partition2 = rounded_gb_partition2 * 1024 * 1024 * 2
+
+# Calculate sectors required for partition 3
+sectors_required_partition3 = kb_partition3 * 2
+
+# Calculate end block for partition 2
+end_block_partition2 = start_block_partition2 + sectors_required_partition2 - 1
+start_block_partition3 = end_block_partition2 + 1
+end_block_partition3 = start_block_partition3 + sectors_required_partition3 - 1
+
+# Print fdisk commands
+print(f"""
+d
+2
+n
+p
+2
+{start_block_partition2}
+{end_block_partition2}
+No
+n
+p
+3
+{start_block_partition3}
+{end_block_partition3}
+n
+e
+{end_block_partition3 + 1}
+n
+
++512M
+n
+
+t
+3
+82
+w
+""")
