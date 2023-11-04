fffb150bdb4d26887d96cd525d7aec480ae74f69
Kim Hendrikse <kim@kiwiclan.com>
2023-11-04 08:24:13 +0100

    Increase scan day to -100 days in case its been turned off for a while

--- cleaner.sh
+++ cleaner.sh
@@ -2,5 +2,5 @@
 
 if findmnt ~/disk > /dev/null && test -d ~/disk/"$(hostname)" > /dev/null ; then
     cd ~/disk/"$(hostname)" || exit 1
-    ~/sbts-bin/clean.sh -f "$(sudo fdisk -l /dev/mmcblk0|awk '$1 == "/dev/mmcblk0p6" {print int($4/2/1024/1024/3.5)+2}')d" -t "$(sudo fdisk -l /dev/mmcblk0|awk '$1 == "/dev/mmcblk0p6" {print int($4/2/1024/1024/3.5)-1}')d"
+    ~/sbts-bin/clean.sh -f "$(sudo fdisk -l /dev/mmcblk0|awk '$1 == "/dev/mmcblk0p6" {print int($4/2/1024/1024/3.5)+100}')d" -t "$(sudo fdisk -l /dev/mmcblk0|awk '$1 == "/dev/mmcblk0p6" {print int($4/2/1024/1024/3.5)-1}')d"
 fi
